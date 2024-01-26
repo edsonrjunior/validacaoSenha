237a2802d2966398f46996171b018a2db081e343
Edson Junior <edsonkjr@gmail.com>
2024-01-26 13:51:21 -0300

    Adicionando projeto

---
+++ validacaoSenha/src/test/java/br/com/edson/validacaoSenha/ValidacaoSenhaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.edson.validacaoSenha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidacaoSenhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
