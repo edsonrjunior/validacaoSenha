335f57295ca5858ba0b4129d24f692d3d0b8c621
Edson Junior <edsonkjr@gmail.com>
2024-01-27 18:40:07 -0300

    Criação de mais testes

--- src/test/java/br/com/edson/validacao_senha/controller/SenhaControllerTest.java
+++ src/test/java/br/com/edson/validacao_senha/controller/SenhaControllerTest.java
@@ -3,45 +3,67 @@ package br.com.edson.validacao_senha.controller;
 
 import br.com.edson.validacao_senha.domain.Senha;
 import br.com.edson.validacao_senha.service.SenhaService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.validation.BindingResult;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
+@WebMvcTest(SenhaController.class)
 class SenhaControllerTest {
 
 
-    @InjectMocks
-    private SenhaController senhaController;
-    @Mock
+    @Autowired
+    private MockMvc mock;
+
+    @MockBean
     private SenhaService senhaService;
 
-    @Mock
-    private BindingResult bindingResult;
-
+    @Spy
+    private BindingResult validacaoSenha;
 
     @Test
-    void testValidacaoSenhaValida() {
-        Senha senha = new Senha("SenhaValida123");
-        when(senhaService.validarSenha(senha, bindingResult)).thenReturn(true);
-        boolean result = senhaController.isValid(senha, bindingResult);
-        assertTrue(result);
+    @DisplayName("Deve retornar http code 200")
+    void deveRetornar200QuandoApiForChamada() throws Exception {
+        var senha = new Senha("AbTp9!fok");
+
+        mock.perform(post("/senha/validarSenha")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(senha)))
+                .andExpect(status().isOk());
     }
 
     @Test
-    void testValidacaoSenhaInvalida() {
-        Senha senha = new Senha("SenhaInvalida");
-        when(senhaService.validarSenha(senha, bindingResult)).thenReturn(false);
-        boolean result = senhaController.isValid(senha, bindingResult);
-        assertFalse(result);
+    @DisplayName("Deve retornar http code 404")
+    void deveRetornar404QuandoAUrlForInvalida() throws Exception {
+        var senha = new Senha("AbTp9!fok");
+
+        mock.perform(post("/senha/urlInvalida")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(senha)))
+                .andExpect(status().isNotFound());
     }
 
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
+
+
